8b417b70d7de4b115b3460d0131da6ed8dfeb3fb
darren-kk <rlawoghks10@gmail.com>
2023-07-19 22:09:54 +0900

    Setting. boilerplate

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+    "import/no-extraneous-dependencies": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/self-closing-comp": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
+    "no-param-reassign": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ src/app/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1 className="text-cyan-300">DATA FACE</h1>
+    </>
+  );
+}
+
+export default App;
