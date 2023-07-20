21bf6fbef4ef583d8e2bc784e44f3a5fac784e09
darren-kk <rlawoghks10@gmail.com>
2023-07-20 14:57:24 +0900

    Fix. file structrue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 
-import App from "./app/App";
+import App from "./components/app/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
