ad5b19a0413c75330407ec10aca5ef26d1d179d6
Youjin-cmd <y.kimiau@gmail.com>
2023-07-21 16:29:41 +0900

    Refactor. main import lines order

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./main.css";
+
 import App from "./components/app/App";
+import "./main.css";
 
 const queryClient = new QueryClient();
 
