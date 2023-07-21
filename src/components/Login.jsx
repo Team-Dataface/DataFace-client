c11d87a48ae7ed09945b3ad7e70bc94818e01641
Youjin-cmd <y.kimiau@gmail.com>
2023-07-21 15:38:23 +0900

    Feat. add login logic to login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,49 @@
-import logo from "../assets/dataface_logo.png";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+import fetchData from "../utils/axios";
+import { firebaseAuth } from "./app/firebaseAuth";
+
 import googleLogo from "../assets/google_logo.png";
+import logo from "../assets/dataface_logo.png";
 
 function Login() {
+  const navigate = useNavigate();
+  const googleProvider = new GoogleAuthProvider();
+
+  async function handleGoogleLogin() {
+    const result = await signInWithPopup(firebaseAuth, googleProvider);
+    const { user } = result;
+
+    const userInfoObject = {
+      email: user.email,
+      username: user.displayName,
+    };
+
+    const response = await fetchData("POST", "/login", userInfoObject);
+  }
+
+  const { mutate, isError, isSuccess } = useMutation(handleGoogleLogin);
+
+  if (isSuccess) {
+    console.log("sending user to dashboard");
+  }
+
+  if (isError) {
+    console.log("sending user to errorpage");
+  }
+
   return (
     <div className="flex flex-col justify-center items-center p-20">
       <img src={logo} alt="logo" className="w-[30rem]" />
       <h1 className="flex justify-center items-center w-full mb-20 text-[3rem]">
         Your Data, Your Way
       </h1>
-      <div className="flex items-center w-[250px] h-[55px] p-1 rounded-[5px] bg-google-blue drop-shadow-md">
+      <button
+        className="flex items-center w-[250px] h-[55px] p-1 rounded-[5px] bg-google-blue drop-shadow-md hover:bg-google-blue-hover"
+        onClick={mutate}
+      >
         <div className="flex justify-center items-center w-[48px] h-[48px] p-15 rounded-[5px] bg-white ">
           <img
             className="w-[18px] h-[18px]"
@@ -16,8 +51,8 @@ function Login() {
             alt="google-login"
           />
         </div>
-        <span className="ml-5 text-white ">Sign in with Google</span>
-      </div>
+        <span className="ml-5 font-roboto text-white">Sign in with Google</span>
+      </button>
     </div>
   );
 }
