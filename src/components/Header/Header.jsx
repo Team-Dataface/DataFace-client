d3cde64c9539c90e78e05610f504e9762a9fc891
darren-kk <rlawoghks10@gmail.com>
2023-07-21 17:20:23 +0900

    UI. fix component inner structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,25 +7,32 @@ import logo from "../../assets/dataface_logo.png";
 
 function Header() {
   return (
-    <div className="flex border-2 w-full">
-      <div className="left w-9/12">
+    <div className="flex flex-col border-2 w-full">
+      <div className="flex flex-row justify-between">
         <div>
           <img className="w-20 h-20" src={logo} alt="dataface logo" />
         </div>
-
-        <div className="flex justify-between">
-          <RelationBtn />
-          <ModifyDocBtns />
-          <ModifyViewBtns />
+        <div>
+          <h1>Page Name</h1>
+        </div>
+        <div className="mx-36">
+          <Button>logout</Button>
         </div>
       </div>
 
-      <div className="right w-3/12 text-center">
-        <div className="my-4">
-          <Button>logout</Button>
+      <div className="flex flex-row justify-between">
+        <div className="left w-9/12">
+          <div className="flex justify-between">
+            <RelationBtn />
+            <ModifyDocBtns />
+            <ModifyViewBtns />
+          </div>
         </div>
-        <div>
-          <Button>save</Button>
+
+        <div className="right w-3/12 text-center">
+          <div>
+            <Button>save</Button>
+          </div>
         </div>
       </div>
     </div>
