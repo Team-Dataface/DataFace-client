e716811d0006737e354e032f68ce96f7c499d4ed
Youjin-cmd <y.kimiau@gmail.com>
2023-07-22 19:51:06 +0900

    Refactor. change google login button to shared component

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import fetchData from "../../utils/axios";
 import { firebaseAuth } from "../../app/firebaseAuth";
 
+import Button from "../shared/Button";
+
 function Login() {
   const navigate = useNavigate();
   const googleProvider = new GoogleAuthProvider();
@@ -39,19 +41,23 @@ function Login() {
       <h1 className="flex justify-center items-center w-full mb-20 text-[3rem]">
         Your Data, Your Way
       </h1>
-      <button
+      <Button
         className="flex items-center w-[250px] h-[55px] p-1 rounded-[5px] bg-google-blue drop-shadow-md hover:bg-google-blue-hover"
         onClick={mutate}
       >
-        <div className="flex justify-center items-center w-[48px] h-[48px] p-15 rounded-[5px] bg-white">
-          <img
-            className="w-[18px] h-[18px]"
-            src="/assets/google_logo.png"
-            alt="google logo"
-          />
-        </div>
-        <span className="ml-5 font-roboto text-white">Sign in with Google</span>
-      </button>
+        <>
+          <div className="flex justify-center items-center w-[48px] h-[48px] p-15 rounded-[5px] bg-white">
+            <img
+              className="w-[18px] h-[18px]"
+              src="/assets/google_logo.png"
+              alt="google logo"
+            />
+          </div>
+          <span className="ml-5 font-roboto text-white">
+            Sign in with Google
+          </span>
+        </>
+      </Button>
     </div>
   );
 }
