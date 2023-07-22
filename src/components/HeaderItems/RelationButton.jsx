222a7aad06eef2e942c3c1f60c4135a31811dfbd
Youjin-cmd <y.kimiau@gmail.com>
2023-07-22 19:57:11 +0900

    UI. basic layout and css setting for Header static

--- src/components/HeaderItems/RelationButton.jsx
+++ src/components/HeaderItems/RelationButton.jsx
@@ -1,7 +1,16 @@
 import Button from "../shared/Button";
 
 function RelationButton() {
-  return <Button className="mx-4">relation</Button>;
+  return (
+    <Button className="flex flex-row items-center w-[130px] h-9 p-2 rounded-md bg-white hover:bg-yellow active:bg-yellow">
+      <img
+        className="ml-1"
+        src="/assets/relation_icon.svg"
+        alt="relation icon"
+      />
+      <span className="w-full">relation</span>
+    </Button>
+  );
 }
 
 export default RelationButton;
--- src/components/HeaderItems/SwitchViewButtons.jsx
+++ src/components/HeaderItems/SwitchViewButtons.jsx
@@ -3,8 +3,14 @@ import Button from "../shared/Button";
 function SwitchViewButtons() {
   return (
     <div className="flex">
-      <Button className="mx-4">list</Button>
-      <Button className="mx-4">detail</Button>
+      <Button className="flex flex-row items-center w-[80px] h-9 mr-1 p-2 rounded-md bg-white hover:bg-yellow active:bg-yellow">
+        <img className="ml-1" src="/assets/list_icon.svg" alt="list icon" />
+        <span className="w-full">list</span>
+      </Button>
+      <Button className="flex flex-row items-center w-[80px] h-9 mr-1 p-2 rounded-md bg-white hover:bg-yellow active:bg-yellow">
+        <img className="ml-1" src="/assets/detail_icon.svg" alt="detail icon" />
+        <span className="w-full">detail</span>
+      </Button>
     </div>
   );
 }
