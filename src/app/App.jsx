25d92b146f4843ca01378c91167e59bb342c8644
Youjin-cmd <y.kimiau@gmail.com>
2023-07-22 19:50:22 +0900

    UI. basic layout setting

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,15 +2,21 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../components/pages/Login";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
-      </Routes>
+      <div className="flex flex-row h-screen">
+        <Sidebar />
+        <div className="flex grow justify-center">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Navigate replace to="/login" />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
