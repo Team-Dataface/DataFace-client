177e55965b781ff39731500c1820f10ab47d0c9b
darren-kk <rlawoghks10@gmail.com>
2023-07-23 16:11:12 +0900

    Fix. fix App component to get user login info

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,35 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+import authUser from "../utils/authUser";
 import Login from "../components/pages/Login";
 import Header from "../components/Header";
+import Dashboard from "../components/pages/Dashboard";
 import Sidebar from "../components/Sidebar";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await authUser();
+        const { success, userId } = response.data;
+
+        if (success) {
+          setUser(userId);
+        } else {
+          setUser("");
+        }
+      } catch (error) {
+        console.log("send user to Error page");
+      }
+    };
+
+    checkLoginStatus();
+  }, [isLoggedIn]);
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -12,7 +37,13 @@ function App() {
         <Sidebar />
         <div className="flex grow justify-center">
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={
+                <Login onSuccess={setUser} setIsLoggedIn={setIsLoggedIn} />
+              }
+            />
+            <Route path="/dashboard" element={<Dashboard user={user} />} />
             <Route path="/" element={<Navigate replace to="/login" />} />
           </Routes>
         </div>
