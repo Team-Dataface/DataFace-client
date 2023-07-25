885c1d38ac724c5251bb4e7686fefba210588398
Youjin-cmd <y.kimiau@gmail.com>
2023-07-25 21:17:59 +0900

    Feat. delete database

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import PropTypes from "prop-types";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
+import PropTypes from "prop-types";
 import fetchData from "../utils/axios";
 
 import Button from "./shared/Button";
@@ -10,6 +10,16 @@ import CreateDBModal from "./Modals/CreateDBModal";
 function Sidebar({ user }) {
   const [showCreateDBModal, setShowCreateDBModal] = useState(false);
 
+  async function deleteDatabase(databaseId) {
+    await fetchData("DELETE", `/users/${user}/databases/${databaseId}`);
+  }
+
+  const { mutate: fetchDeleteDB } = useMutation(deleteDatabase, {
+    onFailure: () => {
+      console.log("sending user to errorpage");
+    },
+  });
+
   async function getDatabaseList() {
     const response = await fetchData("GET", `users/${user}/databases`);
 
@@ -17,6 +27,7 @@ function Sidebar({ user }) {
   }
 
   const { data, isLoading } = useQuery(["userDbList"], getDatabaseList, {
+    enabled: !!user,
     onFailure: () => {
       console.log("sending user to errorpage");
     },
@@ -29,24 +40,24 @@ function Sidebar({ user }) {
   function renderDatabaseList() {
     return data.data.databases.map(element => {
       return (
-        <Button
+        <div
           key={element._id}
           className="flex justify-between items-center w-full hover:bg-grey active:bg-yellow"
         >
-          <div className="flex">
-            <img
-              className="ml-6 mr-2"
-              src="/assets/folder_icon.svg"
-              alt="folder icon"
-            />
-            <span>{element.name}</span>
-          </div>
-          <img
-            className="ml-6 mr-2"
-            src="/assets/bin_icon.svg"
-            alt="bin icon"
-          />
-        </Button>
+          <Button className="w-full">
+            <div className="flex">
+              <img
+                className="ml-6 mr-2"
+                src="/assets/folder_icon.svg"
+                alt="folder icon"
+              />
+              <span>{element.name}</span>
+            </div>
+          </Button>
+          <Button onClick={() => fetchDeleteDB(element._id)}>
+            <img className="mr-2" src="/assets/bin_icon.svg" alt="bin icon" />
+          </Button>
+        </div>
       );
     });
   }
