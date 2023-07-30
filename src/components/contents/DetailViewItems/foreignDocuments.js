3ae0a8024001b27cbcf02e5e297f66b13135484d
Youjin-cmd <y.kimiau@gmail.com>
2023-07-31 02:31:29 +0900

    Setting. add mockup for development

---
+++ src/components/contents/DetailViewItems/foreignDocuments.js
@@ -0,0 +1,104 @@
+const foreignDocuments = [
+  {
+    fields: [
+      {
+        fieldName: "이름",
+        fieldValue: "김유진",
+        _id: "a",
+      },
+      {
+        fieldName: "수업일",
+        fieldValue: "2023.06.07",
+        _id: "b",
+      },
+      {
+        fieldName: "참여학생수",
+        fieldValue: "5",
+        _id: "c",
+      },
+    ],
+    _id: "1234",
+  },
+  {
+    fields: [
+      {
+        fieldName: "이름",
+        fieldValue: "김유진",
+        _id: "d",
+      },
+      {
+        fieldName: "수업일",
+        fieldValue: "2023.07.31",
+        _id: "e",
+      },
+      {
+        fieldName: "참여학생수",
+        fieldValue: "10",
+        _id: "f",
+      },
+    ],
+    _id: "2345",
+  },
+  {
+    fields: [
+      {
+        fieldName: "이름",
+        fieldValue: "김유진",
+        _id: "g",
+      },
+      {
+        fieldName: "수업일",
+        fieldValue: "2023.07.12",
+        _id: "h",
+      },
+      {
+        fieldName: "참여학생수",
+        fieldValue: "14",
+        _id: "i",
+      },
+    ],
+    _id: "0987",
+  },
+  {
+    fields: [
+      {
+        fieldName: "이름",
+        fieldValue: "김유진",
+        _id: "m",
+      },
+      {
+        fieldName: "수업일",
+        fieldValue: "2023.07.29",
+        _id: "n",
+      },
+      {
+        fieldName: "참여학생수",
+        fieldValue: "4",
+        _id: "o",
+      },
+    ],
+    _id: "2375",
+  },
+  {
+    fields: [
+      {
+        fieldName: "이름",
+        fieldValue: "김유진",
+        _id: "p",
+      },
+      {
+        fieldName: "수업일",
+        fieldValue: "2023.08.01",
+        _id: "q",
+      },
+      {
+        fieldName: "참여학생수",
+        fieldValue: "2",
+        _id: "r",
+      },
+    ],
+    _id: "8683",
+  },
+];
+
+export default foreignDocuments;
