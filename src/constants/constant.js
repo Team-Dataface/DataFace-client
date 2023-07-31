54b531935ba14430a521f69b05d73ee37a16358c
Youjin-cmd <y.kimiau@gmail.com>
2023-07-31 18:32:00 +0900

    Fix. remove Number

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,12 +1,5 @@
 const CONSTANT = {
-  FIELD_TYPES: [
-    "Text",
-    "Number",
-    "Date",
-    "Time",
-    "Date created",
-    "Date modified",
-  ],
+  FIELD_TYPES: ["Text", "Date", "Time", "Date created", "Date modified"],
   MAX_DATABASE_NAME_LENGTH: 15,
   MAX_FIELD_NAME_LENGTH: 20,
   ONE_HOUR_IN_MILLISECONDS: 1000 * 60 * 60,
